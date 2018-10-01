bca1a355b2ba2f3f0c629e9deb9af412bd6a02c3
Ali1213 <yusili2000@163.com>
2018-10-02 01:41:20 +0800

    modify

--- front/class/index.js
+++ front/class/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $('.container .main span').click(function(){
         console.log( $(this))
-        $(this).parents('.container').find('.detail').toggle()
+        $(this).parents('.main').toggleClass('main-hasshadow').parents('.container').find('.detail').slideToggle(300)
     })
 
     $('.container .detail').click(function(){
-        $(this).toggle()
+        $(this).slideToggle(300)
+        $(this).parents('.container').find('.main').toggleClass('main-hasshadow')
     })
 })
